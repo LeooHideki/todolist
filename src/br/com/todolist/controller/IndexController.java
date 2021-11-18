1876bd6b444c4e7268256c7885905a602480a834
Leo <leo.deki2000@gmail.com>
2021-11-18 19:48:44 -0300

    Commit Inicial

---
+++ src/br/com/todolist/controller/IndexController.java
@@ -0,0 +1,51 @@
+package br.com.todolist.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class IndexController {
+
+    @FXML
+    private DatePicker dtData;
+
+    @FXML
+    private TextArea tfObs;
+
+    @FXML
+    private TextField tfDescricao;                                       
+
+    @FXML
+    private TextField tfStatus;
+
+    @FXML
+    private TextField tfNome;
+
+    @FXML
+    void btAdiar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btApagar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btConcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btLimpar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btSalvar(ActionEvent event) {
+
+    }
+
+}
